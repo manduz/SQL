19beb024e2f49bbc6c4626fcf8fa413c59fe303e
manduz <emekmanduz3623@gmail.com>
2024-10-06 17:14:23 +0300

    update kullanımı

---
+++ Update.sql
@@ -0,0 +1,13 @@
+--update
+--update Tabloadi set kolon1= YeniDeger, Kolon2 = YeniDeger
+--where
+
+update Personel1
+set
+Soyisim = 'Ali',  --tablo ismiyle ayný olmalý
+Isým = 'Veli'
+
+where --nerde yapacaðým güncellemeyi
+PersonelID = 5
+
+select * from Personel1
