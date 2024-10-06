5a8b1a451772155bbf208463fc23b4a0db0529f0
manduz <emekmanduz3623@gmail.com>
2024-10-06 18:07:18 +0300

    where ve tablo kolon ismi değişikliği

--- AdventureDB_SelectVeriSorgulama.sql
+++ AdventureDB_SelectVeriSorgulama.sql
@@ -1,1 +1,9 @@
 Select * from DimProduct
+
+
+--sadece kolon isimlerinde deðiþiklik yapar ProductAlternateKe bu ismin yerine UrunAnahtar, yazar EnglishProductName AS [Ingiliz Anahtar AdI] ayný þekilde deðiþtirri
+select ProductAlternateKey as UrunAnahtar, --ana tabloda bir þey deðiþtirmez
+EnglishProductName AS [Ingiliz Anahtar AdI]
+from DimProduct  --select sutunadi
+
+
---
+++ whereKullanımı.sql
@@ -0,0 +1,5 @@
+--where i verileri getirmek istediðim kriterlere göre kullanýlan bir yapýdýr.
+
+select * from DimProduct
+where ProductKey = '1' --key i 1 olaný getir
+
