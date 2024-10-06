d2dacfb989b87f9c8097fee8b21b24ac67e39832
manduz <emekmanduz3623@gmail.com>
2024-10-06 18:15:42 +0300

    yorum satırına almak

---
+++ yorumsatırı.sql
@@ -0,0 +1,11 @@
+/*Select all the colums 
+of all the records 
+in the Customer table*/
+
+--select * from Products --where City='Berlin'
+
+--select * from Products
+select ContactName/*,City*/ from Products
+/*select * from Products
+select * from Products
+select * from Products*/
