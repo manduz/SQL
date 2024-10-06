075efec6f9cf86d2ad55d15867de5e8a93727094
manduz <emekmanduz3623@gmail.com>
2024-10-06 18:11:33 +0300

    and ve or kullanımı

---
+++ And_Or.sql
@@ -0,0 +1,12 @@
+select * from DimProduct
+
+
+select * from DimProduct
+where
+color = 'Yellow'
+and ProductSubcategoryKey = '14'
+
+select * from DimProduct
+where
+color = 'Yellow'
+or color = 'Blue'
