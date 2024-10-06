669378c338d303bf8ab6855321a2202d2de88b9d
manduz <emekmanduz3623@gmail.com>
2024-10-06 17:41:46 +0300

    Veri Silme

---
+++ VeriSilme.sql
@@ -0,0 +1,16 @@
+--verileri silmek için delete komutu kullanýlýr
+
+--delete TabloAdi where kriter ikelinde kod satýrý var
+
+
+update Personel1
+set
+PersonelID = 2  --email adresi emek@gmail.com olan mailin Personel Id sini 2 yap
+where
+EmailAdres = 'emek@gmail.com'
+
+
+
+
+Select * from Personel1
+
