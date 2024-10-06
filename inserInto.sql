360de82629450441f01f5d1624816ddaa02dd33d
manduz <emekmanduz3623@gmail.com>
2024-10-06 17:09:07 +0300

    insert into özelliği

---
+++ inserInto.sql
@@ -0,0 +1,8 @@
+--tabloya veri eklemek istiyorsam insert into kullanýlýr.
+--genel kullanýmý; insert into TabloAdi(kolon1,kolon2,...) values (deger1,deger2,degern) þeklinde kullanýlýr
+--2. genel kullanýmý; inseet into TabloAdi values(deger1,deger2,..,degern) þeklinde kolona karþýlýk gelen deðerler
+
+
+insert into Personel1 (PersonelID, Isým,Soyisim,EmailAdres) values(5,'Emek','Manduz','emekmanduz3623@gmail.com')
+
+insert into Personel1 values (3,'emek','bakkal','emek@gmail.com')
