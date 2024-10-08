4694ad29faa9593ff0a74bf5981f50d40fa71ec5
manduz <emekmanduz3623@gmail.com>
2024-10-08 16:41:30 +0300

    count average

---
+++ count_average_kullanimi.sql
@@ -0,0 +1,13 @@
+---select count(kolom) from Tablo where þart..
+
+select count(*) from DimProduct
+where
+color = 'Black'
+
+
+select * from DimProduct
+
+select AVG(SafetyStockLevel) from DimProduct
+where color = 'Black'
+
+
