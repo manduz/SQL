0cead5462895c0e82f2a4e5ae1f0cfaaa2439630
manduz <emekmanduz3623@gmail.com>
2024-10-12 22:33:34 +0300

    Update sqlOrnek2.sql

--- sqlOrnek2.sql
+++ sqlOrnek2.sql
@@ -1,4 +1,15 @@
 -- FactinternetSale tablosdaki hangi ürünü hangi müþteriye ne kadar atmýþýz
 
+select DP.EnglishProductName,
+DC.FirstName,--hangi ürün
+DC.LastName,
+FIS.TotalProductCost
+from FactInternetSales as FIS
+inner join DimProduct as DP
+on FIS.ProductKey = DP.ProductKey
+inner join DimCustomer as DC
+on FIS.CustomerKey = DC.CustomerKey
+
+
 
 
