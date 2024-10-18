59e65c64ac0d6154ca3918e49b45074c81c093b3
manduz <emekmanduz3623@gmail.com>
2024-10-19 00:25:57 +0300

    tablo ekle çıkar kolon ekle çıkar modify

--- tablo_silme.sql
+++ tablo_silme.sql
@@ -3,5 +3,18 @@
 --drop table TabloAdi
 
 
---tablo güncelleme yapýlmasý için alter kullanýyoruz
+--tablo kolon ekleme çýkarma gibi güncelleme yapýlmasý için alter kullanýyoruz
+
+alter table People
+add Email varchar(50)
+
+--kolon çýkar
+alter table People
+drop Column Email
+
+
+--modify etmek için
+alter table People
+alter column LastName varchar(MAX)
+
 
