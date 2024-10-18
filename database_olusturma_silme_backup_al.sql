ed9ae14fb2451e3b80df90f5ca2d45736f6740e8
manduz <emekmanduz3623@gmail.com>
2024-10-18 23:46:23 +0300

    database create delete backup

---
+++ database_olusturma_silme_backup_al.sql
@@ -0,0 +1,10 @@
+--create database emek
+
+---silmek için,üzerine gelip  silmek daha doðru
+--drop database emek
+
+-----------------------------------------------
+--back up iþlemi
+backup database emek to disk = 'C:\Program Files\Microsoft SQL Server\MSSQL16.SQLKODLAB\MSSQL\Backup\emek2.bak'
+--eger silersen database i daha sonra Databases sekmesine gelip sað týklayýp Restore Database e týklayýp backup aldýðýn ve sildiðin databesi getirebiliyorsun
+
