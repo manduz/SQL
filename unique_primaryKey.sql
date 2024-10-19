1ab32f347a0729dec60837021f82897bf773c164
manduz <emekmanduz3623@gmail.com>
2024-10-19 23:56:38 +0300

    unique primary key

---
+++ unique_primaryKey.sql
@@ -0,0 +1,15 @@
+
+create table Emek2
+(
+	ID int not null unique,
+	LastName varchar(50) not null
+)
+
+create table Emek3
+(
+	ID int not null,
+	Age int
+	primary key(ID)--hem sadece bir kez ayný deðeri girebilriisn hem de baþka bir tablo ile baðlamýþ olursun
+)
+
+
